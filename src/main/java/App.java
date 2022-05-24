d6d6248f1bcd0f619d1dee12a52dcff0d28f24b3
lchahua <62721646+edededd@users.noreply.github.com>
2022-05-24 19:42:18 -0400

    inicio

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
+import java.util.logging.Logger;
 public class App {
-    
+    static Logger logger= Logger.getLogger(App.class.getName());
     public static void main(String[] args){
-        
+       logger.info("SE1");
     }
 }
